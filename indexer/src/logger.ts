1e316160a6144e8201749ea325815d590c6df208
Raghav-28-Gupta <raghavgupta07159@gmail.com>
2025-11-20 19:31:54 +0530

    standardized formatting in config and logger files

--- indexer/src/logger.ts
+++ indexer/src/logger.ts
@@ -1,30 +1,33 @@
-import pino from 'pino';
-import { config } from './config.js';
+import pino from "pino";
+import { config } from "./config.js";
 
 export const logger = pino({
-     level: config.logging.level,
-     transport: {
-          target: 'pino-pretty',
-          options: {
-               colorize: true,
-               translateTime: 'HH:MM:ss Z',
-               ignore: 'pid,hostname',
-          },
-     },
+	level: config.logging.level,
+	transport: {
+		target: "pino-pretty",
+		options: {
+			colorize: true,
+			translateTime: "HH:MM:ss Z",
+			ignore: "pid,hostname",
+		},
+	},
 });
 
-export function logEvent(type: 'Deposit' | 'Withdraw', data: {
-     txHash: string;
-     chainId: number;
-     blockNumber: number;
-     token: string;
-     amount: string;
-}) {
-     logger.info(
-          {
-               eventType: type,
-               ...data,
-          },
-          `${type} event indexed`
-     );
+export function logEvent(
+	type: "Deposit" | "Withdraw",
+	data: {
+		txHash: string;
+		chainId: number;
+		blockNumber: number;
+		token: string;
+		amount: string;
+	}
+) {
+	logger.info(
+		{
+			eventType: type,
+			...data,
+		},
+		`${type} event indexed`
+	);
 }
